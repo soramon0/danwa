f3e98a0881e1115146cc3c16aa1df9f9998def29
KarimLa <karimacc001@gmail.com>
2021-07-23 14:20:12 +0100

    added auth with laravel/ui

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ThreadController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +19,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Auth::routes();
+Route::get('/home', [HomeController::class, 'index'])->name('home');
